ed1947f00f10698824146e144bf65309845e80ef
zhihuifang <1991738816@qq.com>
2017-05-01 17:39:42 +0800

    修改购物车
    1、增加商品详情界面
    2、增加商品件数总计
    3、增加商品加入购物车功能
    4、增加商品库存以及加减
    5、增加商品描述属性

--- js/dataService.js
+++ js/dataService.js
@@ -3,13 +3,14 @@ angular.module("ShopApp")
         // 内部变量
         var self = this;
         items = [
-            {name: "海尔电视", price: 2000, quantity: 2, sum:0},
-            {name: "长虹电视", price: 3000, quantity: 1, sum:0},
-            {name: "联想电视", price: 4000, quantity: 3, sum:0},
+            {name: "海尔电视", price: 2000, quantity: 0, sum:0, inventory:100, describe:"海尔电视是一款高清便宜性价比高的电视"},
+            {name: "长虹电视", price: 3000, quantity: 0, sum:0, inventory:100, describe:"长虹电视是一款高清便宜性价比高的电视"},
+            {name: "联想电视", price: 4000, quantity: 0, sum:0, inventory:100, describe:"联想电视是一款高清便宜性价比高的电视"},
         ];
 
         // 对外接口：数据
         self.items = items;
+        self.totalCount = 0;
         self.totalAmount = 0; //这个既是内部使用，也是外部使用，因为他不是地址引用的对象， 只是简单的值变量
 
         // 对外接口：处理函数
@@ -19,6 +20,7 @@ angular.module("ShopApp")
         // 内部具体实现
         function add(index) {
             self.items[index].quantity++;
+            self.items[index].inventory--;
             $timeout(update);
         }
 
@@ -33,10 +35,12 @@ angular.module("ShopApp")
                 item;
 
             self.totalAmount = 0;
+            self.totalCount = 0;
             for (i = 0; i < n; i++) {
                 item = self.items[i];
                 item.sum = item.price * item.quantity;
                 self.totalAmount += item.sum;
+                self.totalCount += item.quantity;
             }
         }
         $timeout(update);
