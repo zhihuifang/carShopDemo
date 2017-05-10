79d2a8d7bab5d613b97d6f5fbc24d8677bdb44ea
zhihuifang <1991738816@qq.com>
2017-05-10 09:51:37 +0800

    修改index未定义
    1、将函数定义到服务中

--- js/goodsController.js
+++ js/goodsController.js
@@ -4,16 +4,4 @@ angular.module("ShopApp")
         var self = this;
         self.data = DataService;
         self.goods = DataService.goods;
-        self.selectGoods = selectGoods;
-        var selectGoods = [];
-        var selected = self.goods[index];
-        var selectGood = {
-            name: selected.name,
-            price: selected.price,
-            inventory:selected.inventory,
-            describe:selected.describe,
-            sum: 0,
-            quantity: 1
-        };
-        self.selectGoods.push(selectGood);
 });
