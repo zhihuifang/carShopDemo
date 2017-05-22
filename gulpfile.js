35fc1af30b2cf34b4eb286be9e9b96ae71c67402
zhihuifang <1991738816@qq.com>
2017-05-22 19:14:24 +0800

    合并js，css文件

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,13 @@ var args = require('yargs').argv;
 var runSequence = require('run-sequence');
 JSON.minify = JSON.minify || require("node-json-minify");
 
-var APP_JS_FILE = "demo-app-all.js",
-    APP_JS_MIN_FILE = "demo-app-all.min.js",
-    APP_JS_MAP = "demo-app-all.map",
-    DIST_FOLDER = "dist/demoApp/";
+var APP_JS_FILE = "car-app-all.js",
+    APP_JS_MIN_FILE = "car-app-all.min.js",
+    APP_JS_MAP = "car-app-all.map",
+    APP_CSS_FILE = "car-app-all.CSS",
+    APP_CSS_MIN_FILE = "car-app-all.min.CSS",
+    APP_CSS_MAP = "car-app-all.map",
+    DIST_FOLDER = "dist/carShop/";
 
 gulp.task('clean', del.bind(null, ['dist']));
 
@@ -22,6 +25,7 @@ gulp.task('concat-files', function () {
         .pipe(assets.restore())
         .pipe($.useref())
         .pipe($.if('*.html', $.replace(APP_JS_FILE, APP_JS_MIN_FILE)))
+        .pipe($.if('*.html', $.replace(APP_CSS_FILE, APP_CSS_MIN_FILE)))
         .pipe(gulp.dest(DIST_FOLDER));
 });
 
@@ -34,9 +38,17 @@ gulp.task('minify', function () {
             warnings: false
         }
     });
+    var result2 = uglifyjs.minify([DIST_FOLDER + APP_CSS_FILE], {
+        outSourceMap: APP_CSS_MAP,
+        sourceRoot: "",
+        compress: {
+            warnings: false
+        }
+    });
     result.map = result.map.replace('"sources":[' + DIST_FOLDER, '"sources":["');
     fs.writeFileSync(DIST_FOLDER + APP_JS_MAP, result.map);
-    fs.writeFileSync(DIST_FOLDER + APP_JS_MIN_FILE, result.code);
+    fs.writeFileSync(DIST_FOLDER + APP_CSS_MAP, result.map);
+    fs.writeFileSync(DIST_FOLDER + APP_JS_MIN_FILE + APP_CSS_MIN_FILE, result.code);
 });
 
 gulp.task('update-index', function () {
